8faec1218d84708e3c55a7b1f6445ed152a518f2
Taha Rawjani <tahakazim08@gmail.com>
2026-02-13 22:00:22 -0500

    initial

--- server/agents/Carmack.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    generateCode: async (design, assets) => {
-        console.log(`[CARMACK] Generating Phaser.js game code for ${design.title}...`);
-        return { scripts: ["main.js", "GameScene.js", "Preloader.js"] };
-    }
-};
--- server/agents/Skinner.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    setupAnalytics: async (goals) => {
-        console.log(`[SKINNER] Configuring analytics for success metrics: ${goals.join(", ")}`);
-        return { trackingId: "UA-NEXUS-001", events: ["concept_mastered", "mechanics_interaction"] };
-    }
-};
